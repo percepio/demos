3c7abadaab46b1630ba36c9b4169043abab04de4
Johan <johan.kraft@percepio.com>
2025-08-03 20:30:37 +0200

    Enabled trace payloads

--- PIC32CK-SG01/firmware/src/PercepioProjectConf/dfmCrashCatcherConfig.h
+++ PIC32CK-SG01/firmware/src/PercepioProjectConf/dfmCrashCatcherConfig.h
@@ -45,7 +45,7 @@ extern "C" {
 /**
  * @brief If this is set to 1 it will attempt to also save a trace with the Alert. This requires the Percepio Trace Recorder to also be included in the project.
  */
-#define DFM_CFG_CRASH_ADD_TRACE	 0
+#define DFM_CFG_CRASH_ADD_TRACE	1
 
 #ifdef __cplusplus
 }
